c2afcdbc3143b62bb938d7afe38c7f70758a7712
Mayank697 <smayank2000@gmail.com>
2021-12-24 23:55:04 +0530

    home-page-about-setup

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
+import Navbar from "./component/Navbar";
+import About from "./component/About";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <>
+    <Router>
+      <Navbar />
+        <Switch>
+          <Route exact path="/about">
+            <About />
+          </Route>
+        </Switch>
+    </Router>    
+    </>
   );
 }
 
