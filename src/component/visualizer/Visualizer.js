001b7bfc0123c69d3ce93415e6bde65e847237c4
Mayank697 <smayank2000@gmail.com>
2022-01-04 22:47:34 +0530

    fixed-varibale-bugs

--- src/component/visualizer/Visualizer.js
+++ src/component/visualizer/Visualizer.js
@@ -1,12 +1,9 @@
 import React from 'react';
-import { useState } from 'react';
 import { useEffect } from 'react';
 import '../../static/style/bar.css';
 
 
 export default function Visualizer(props) {
-    
-    const [initialrray, setArray] = useState([]);
     
     useEffect(() => {
         initializeArrayBars();
